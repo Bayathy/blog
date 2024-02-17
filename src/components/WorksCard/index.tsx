6f8b6befe5d4faf803b1f54721f41cd5998108d0
Bayathy <misosoup0321@icloud.com>
2024-02-17 19:35:21 +0900

    fix: 🐞 カードのスタイルの修正

--- src/components/WorksCard/index.tsx
+++ src/components/WorksCard/index.tsx
@@ -7,10 +7,10 @@ import Link from 'next/link'
 import { Card, CardContent, CardHeader } from '../ui/card'
 import type { Work } from '@/types/work'
 
-export const WorksCard: FC<Work> = ({ _id, title, description, Image }) => {
+export const WorksCard: FC<Work> = ({ _id, title, Image }) => {
   return (
     <Link href={`/works/detail/${_id}`}>
-      <Card>
+      <Card className="bg-secondary/50 text-blue-600 backdrop-blur-xl duration-200 ease-in hover:bg-secondary/70 dark:text-blue-400 ">
         <CardHeader>
           <AspectRatio ratio={16 / 9}>
             <NextImage src={Image.src} alt={Image.alt ?? title} className="object-contain" fill />
