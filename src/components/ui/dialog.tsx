542b54674d570a9de1620bb063bb056ccea54af9
Bayathy <misosoup0321@icloud.com>
2024-02-21 12:14:04 +0900

    feat: 🚀 スタイルの修正

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -5,6 +5,7 @@ import * as DialogPrimitive from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
 
 import { useRouter } from 'next/navigation'
+import { Button } from './button'
 import { cn } from '@/lib/utils'
 
 const Dialog = DialogPrimitive.Root
@@ -55,10 +56,10 @@ const DialogContent = React.forwardRef<
           >
             {children}
             <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" asChild>
-              <button onClick={() => routerBack()}>
+              <Button className="size-[32px]" size="icon" onClick={() => routerBack()}>
                 <X className="size-4" />
                 <span className="sr-only">Close</span>
-              </button>
+              </Button>
             </DialogPrimitive.Close>
           </DialogPrimitive.Content>
         </DialogPortal>
